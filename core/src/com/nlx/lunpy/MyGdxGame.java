2a78fb79ed9e20df6562c787c8c267ea13cf1fa6
NevLax <qwegornikitin@gmail.com>
2022-09-19 00:56:46 +0400

    Add viewport

--- core/src/com/nlx/lunpy/MyGdxGame.java
+++ core/src/com/nlx/lunpy/MyGdxGame.java
@@ -1,12 +1,16 @@
 package com.nlx.lunpy;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -15,6 +19,8 @@ public class MyGdxGame extends ApplicationAdapter {
 	World world;
 	Box2DDebugRenderer debugRenderer;
 	SimplePhisikBody simpleBody;
+	Viewport viewport;
+	Camera camera;
 	
 	@Override
 	public void create () {
@@ -24,6 +30,9 @@ public class MyGdxGame extends ApplicationAdapter {
 		world = new World(Vector2.Zero, true);
 		debugRenderer = new Box2DDebugRenderer();
 		simpleBody = new SimplePhisikBody(world);
+		camera = new OrthographicCamera();
+		viewport = new ExtendViewport(1280, 720, camera);
+
 	}
 
 	@Override
@@ -33,10 +42,13 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
-		//debugRenderer.render(world, camera.combined);
-
+		debugRenderer.render(world, camera.combined);
 	}
-	
+
+	public void resize(int width, int height) {
+		viewport.update(width, height);
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
